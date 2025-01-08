c3b66465616856b9fe6c034c8edf7b3210c71072
Wojtek Staszewski <wojciechstaszewski4@gmail.com>
2025-01-08 01:03:59 +0100

    Add files via upload
    Signed-off-by: Wojtek Staszewski <wojciechstaszewski4@gmail.com>

---
+++ AmusementParkDB/Program.cs
@@ -0,0 +1,38 @@
+using System.Globalization;
+using AmusementParkDB.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Enforce an “en-US” culture for the entire application.
+var cultureInfo = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+// Adding a database context.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AmusementParkDbContext>(options => options.UseSqlServer(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
